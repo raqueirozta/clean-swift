52808f4aafe08a86405b870d06f67d775e241cf5
Raphael <raqueirozta@gmail.com>
2021-07-21 20:03:50 -0300

    Initial Commit

---
+++ clean-swiftTests/clean_swiftTests.swift
@@ -0,0 +1,33 @@
+//
+//  clean_swiftTests.swift
+//  clean-swiftTests
+//
+//  Created by Raphael on 21/07/21.
+//
+
+import XCTest
+@testable import clean_swift
+
+class clean_swiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
