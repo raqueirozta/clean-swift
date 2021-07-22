dd9aef2828eb8af651058cbb27058d21ee52450b
Raphael <raqueirozta@gmail.com>
2021-07-21 22:01:08 -0300

    feat: Create Initial ViewController

--- clean-swift/ViewController.swift
+++ clean-swift/ViewController.swift
@@ -1,19 +1,29 @@
-//
-//  ViewController.swift
-//  clean-swift
-//
-//  Created by Raphael on 21/07/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var sendButton: UIButton!
+    var name = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makeInitialSetupView()
     }
-
-
+      
+    func makeInitialSetupView() {
+        self.resultLabel.text = ""
+        self.sendButton.setTitle("Enviar", for: .normal)
+    }
+    func makeFinalView() {
+        self.sendButton.setTitle("Enviado", for: .normal)
+        self.nameTextField.text = ""
+    }
+    		
+    @IBAction func buttonAction(_ sender: Any) {
+        name += nameTextField.text!
+        self.resultLabel.text = name
+        makeFinalView()
+    }
 }
 
